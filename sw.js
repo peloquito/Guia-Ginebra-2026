cbb5955fd759137652afb5f1db30d7e2dc3af26c
peloquito <peloquito@me.com>
2026-09-20 18:28:12 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'guia-ginebra-2026-v2-visual';
+const CORE = ['./', './index.html', './manifest.webmanifest', './apple-touch-icon.svg', './icon-192.svg', './icon-512.svg'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  const requestURL = new URL(event.request.url);
+  if (requestURL.origin !== self.location.origin) {
+    event.respondWith(fetch(event.request).catch(() => new Response('', {status: 503, statusText: 'Offline'})));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match('./index.html'))));
+});
